5eea096dd0978d7b283b2f977f548d826b913e88
mrent <orbeq.rentaro.murakami@gmail.com>
2023-09-07 23:08:29 +0900

    割った枚数に応じてリザルト画面を変化するようにした。 割った枚数が14以下なら普通のリザルトに15以上なら豪華なリザルトになるように設定した。

--- Assets/Scenes/script/GameManager.cs
+++ Assets/Scenes/script/GameManager.cs
@@ -48,13 +48,16 @@ public class GameManager : MonoBehaviour
     {
         // ここに制限時間が終了したときの処理を書く
         PlayerPrefs.SetInt("WoodCount", woodCount);//薪の数をリザルト画面に渡す。
-        ToResult();
+
+        //薪の数によってリザルト画面の表示を変更する。
+        if(woodCount>=15){//薪を割った数が15以上なら豪華リザルトに遷移
+            SceneManager.LoadScene("RichResult");//豪華リザルト画面に遷移
+        }
+         if(woodCount<=14){//薪を割った数が14以下なら通常リザルトに遷移
+            SceneManager.LoadScene("Result");//リザルト通常画面に遷移
+        }
     }
 
-    private void ToResult()//リザルト画面に遷移
-    {
-        SceneManager.LoadScene("Result");
-    }
     
      //時間をUI Textに表示するメソッド
     private void UpdateTimeText()
