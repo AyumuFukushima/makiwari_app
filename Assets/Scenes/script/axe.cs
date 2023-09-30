c53abd700a7dc443ce5c6358de9557bfa3be83a4
mrent <orbeq.rentaro.murakami@gmail.com>
2023-09-30 18:55:58 +0900

    ケンさんモーションの仕様の変更

--- Assets/Scenes/script/axe.cs
+++ Assets/Scenes/script/axe.cs
@@ -10,7 +10,7 @@ public class axe : MonoBehaviour
     
     [SerializeField]
     [Tooltip("壊れた薪のプレハブを設定")]
-    private GameObject brokenWoodPrefab; // 壊れた薪のプレハブ
+    private GameObject brokenWoodPrefab; //壊れた薪のプレハブ
 
     private GameObject woodObj;
 
@@ -30,22 +30,22 @@ public class axe : MonoBehaviour
     private IEnumerator OnCollisionEnter2D(Collision2D Collision){//斧が薪に当たった時の処理
         if(Collision.gameObject.name=="wood")//斧が薪に当たっとき
         {
-        Vector3 woodPosition = Collision.transform.position;// 壊れた薪の位置に新しい薪を生成
-
+        Vector3 woodPosition = Collision.transform.position;// 壊れた薪の位置に新しい薪の位置を決める。
+        
         // オフセットを設定して新しい薪の位置を計算
         Vector3 newWoodPosition = woodPosition + new Vector3(-0.21f, -0.3f, 0f); // 調整
 
         Destroy(Collision.gameObject);
-        GameObject newWood = Instantiate(brokenWoodPrefab, newWoodPosition, Quaternion.identity);
+        GameObject newWood = Instantiate(brokenWoodPrefab, newWoodPosition, Quaternion.identity);//割れた薪の生成
+
 
             GameManager GameManager = FindObjectOfType<GameManager>();
             if (GameManager != null)
             {
                 GameManager.IncreaseWoodCount(); // 薪の数を増やす
             }
-
             yield return new WaitForSeconds(respawnTime);//respawnTIme待つ
-                        
+
             Destroy(newWood);// 生成された新しい薪を削除
             SpawnWood();//生成
         }
