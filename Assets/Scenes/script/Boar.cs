4e167ae50459b8f3b492643ce500877dfede7768
mrent <orbeq.rentaro.murakami@gmail.com>
2023-10-09 14:38:20 +0900

    ヘビ実装 イノシシとヘビ当たり判定調整

--- Assets/Scenes/script/Boar.cs
+++ Assets/Scenes/script/Boar.cs
@@ -1,6 +1,7 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+
 public class Boar : MonoBehaviour
 {
     private Rigidbody2D rbody2D;
@@ -10,15 +11,19 @@ public class Boar : MonoBehaviour
     float animTime = 1.0f;//アニメーション再生時間
     Animator boarAnim = null;//いのししのアニメーションの空
     GameObject fx;
+    float fxPositionX=1.0f;//爆発のX座標ずらす距離
+
     void Start()
     {
         rbody2D = GetComponent<Rigidbody2D>();
         fx = (GameObject)Resources.Load("Prefabs/Explode");
     }
+
     void FixedUpdate()
     {
         rbody2D.velocity = new Vector2(speed, rbody2D.velocity.y);
     }
+    
     private void OnCollisionEnter2D(Collision2D other)
     {
         if (other.gameObject.CompareTag("Player"))
@@ -27,19 +32,20 @@ public class Boar : MonoBehaviour
             flagReloadTime = Time.time;//当たった時の時間
             Destroy(this.gameObject);
             VibrationMng.ShortVibration();//スマホ振動実行
-            Instantiate(fx, new Vector3(rbody2D.transform.position.x, rbody2D.transform.position.y), Quaternion.identity);
+            Instantiate(fx, new Vector3(rbody2D.transform.position.x+fxPositionX, rbody2D.transform.position.y), Quaternion.identity);
         }
         if (other.gameObject.CompareTag("Catch"))
         {
             Destroy (this.gameObject);
         }
     }
+
     public void OnClickBoar()//いのししやられアニメーション再生
     {
         boarAnim=this.gameObject.GetComponent<Animator>();//いのししアニメーション取得
         StartCoroutine(BoarTap());
     }
-        IEnumerator BoarTap()
+    IEnumerator BoarTap()
     {
         speed *= 0;
         boarAnim.SetBool("boarTap", true);//やられたときのモーションを再生
--- Assets/Scenes/script/SnakeGenerator.cs
+++ Assets/Scenes/script/SnakeGenerator.cs
@@ -2,21 +2,21 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class BoarGenerator : MonoBehaviour
+public class SnakeGenerator : MonoBehaviour
 {
     public float minGenerateFrame;//最小出現間隔
     public float maxGenerateFrame;//最大出現間隔
 
-    GameObject boar;
-    Vector3 boarPosition;
+    GameObject snake;
+    Vector3 snakePosition;
     int generateFrame;
     int frame = 0;
 
     void Start()
     {
-        boar = (GameObject)Resources.Load("Prefabs/eto_remake_inoshishi");
-        boarPosition = Camera.main.ViewportToWorldPoint(new Vector3(-0.1f, 0.25f));
-        boarPosition.z = 0;
+        snake = (GameObject)Resources.Load("Prefabs/hebi");
+        snakePosition = Camera.main.ViewportToWorldPoint(new Vector3(-0.1f, 0.24f));
+        snakePosition.z = 0;
     }
 
     void Update()
@@ -26,7 +26,7 @@ public class BoarGenerator : MonoBehaviour
 
         if (frame > generateFrame)
         {
-            Instantiate(boar, boarPosition, Quaternion.identity);//イノシシ生成
+            Instantiate(snake, snakePosition, Quaternion.identity);//ヘビ生成
             frame = 0;
         }
     }
